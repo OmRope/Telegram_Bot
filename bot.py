3a61aaf977e38289f3efea8f1c46c83d1aa65d18
Om Rope <omrope69@gmail.com>
2025-01-30 16:52:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from handlers.registrations import start, handle_contact
 from handlers.chat import chat_with_gemini
 from handlers.image_analysis import analyze_file
 from handlers.web_search import web_search
+from handlers.translate import translate_text
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -18,6 +19,7 @@ def main():
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat_with_gemini))
     application.add_handler(MessageHandler(filters.PHOTO | filters.Document.ALL, analyze_file))
     application.add_handler(CommandHandler("websearch", web_search))
+    application.add_handler(CommandHandler("translate", translate_text))
 
     print("Bot running")
     application.run_polling()
